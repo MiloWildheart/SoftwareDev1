266f8e1b48fe1182e340abc9d75bea63da0eaf2e
Robin Kaarsgaren <s1184798@student.windesheim.nl>
2023-04-11 23:22:22 +0200

    initial commit

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Library.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
