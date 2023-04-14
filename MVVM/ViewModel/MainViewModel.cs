8b0ab3a6e8e89ccc4e172ebbd6f92ee58d8f243e
Robin Kaarsgaren <s1184798@student.windesheim.nl>
2023-04-14 20:12:58 +0200

    mostly together, just need to add comments

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,24 @@ namespace Library.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public HomeViewModel HomeVM { get; set; }
+        
+        private object _currentView;
+
+        public object currentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
         public MainViewModel()
         {
-
+            HomeVM= new HomeViewModel();
+            currentView = HomeVM;
         }
     }
 }
